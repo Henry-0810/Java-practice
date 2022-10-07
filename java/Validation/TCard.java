a5f7ee952bb25f1a0c4a5ce83b1ebd7a3f1eb9b5
T00229172 <mun.li.pan@students.ittralee.ie>
2022-10-07 12:37:00 +0100

    validation done

--- java/Validation/TCard.java
+++ java/Validation/TCard.java
@@ -1,28 +1,32 @@
-package Validation;
-
 import javax.swing.*;
 
 public class TCard {
     public static void main(String[] args) {
-        String tNo = JOptionPane.showInputDialog("Please enter a t-number (return to exit)");
-        if(tNo.isEmpty()){
-            JOptionPane.showMessageDialog(null,"You`entered no valid t-numbers!",
-                    "Valid t-numbers",1);
-        }
-        else if(tNo.length() == 9){
-            if( tNo.charAt(0)=='t' || tNo.charAt(0)=='T'){
-                for(int i = 1; i<tNo.length(); i++){
-                    if(Character.isDigit(tNo.charAt(i))){
-                        JOptionPane.showMessageDialog(null,"Valid t-Number",
-                                "Valid t-numbers",1);
+        int counts = 3;
+        String tNo;
+        String output = "Number of valid t-numbers entered: " + counts + "\nList of valid t-numbers:\n\n";
+        StringBuilder outputCont = new StringBuilder();
+        while(counts < 6) {
+            tNo = JOptionPane.showInputDialog("Please enter a t-number (return to exit)");
+            if (tNo == null || tNo.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "You`entered no valid t-numbers!",
+                        "Valid t-numbers", 1);
+            } else if (tNo.length() == 9) {
+                if (tNo.charAt(0) == 't' || tNo.charAt(0) == 'T') {
+                    for (int i = 1; i < tNo.length(); i++) {
+                        if (Character.isDigit(tNo.charAt(i))) {
+                            outputCont.append(tNo).append("\n");
+                            break;
+                        }
                     }
                 }
+            } else {
+                JOptionPane.showMessageDialog(null, "You`entered no valid t-numbers!",
+                        "Valid t-numbers", 1);
             }
+            counts++;
         }
-        else{
-            JOptionPane.showMessageDialog(null,"You`entered no valid t-numbers!",
-                    "Valid t-numbers",1);
-        }
-
+        JOptionPane.showMessageDialog(null, output + outputCont,
+                "Valid t-numbers", 1);
     }
 }
